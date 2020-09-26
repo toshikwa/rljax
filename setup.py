a5ad3c43357153d4d570db87f1e239197c6f0ef0
Toshiki Watanabe <kuboy2482@gmail.com>
2020-09-27 07:47:33 +0900

    bump v0.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = "https://github.com/ku2482/rljax"
 EMAIL = "watanabe.toshiki@outlook.jp"
 AUTHOR = "Toshiki Watanabe"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 here = os.path.abspath(os.path.dirname(__file__))
 REQUIRED = open(os.path.join(here, "requirements.txt")).read().splitlines()
