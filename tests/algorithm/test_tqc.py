c2b08c1d691601ab798dff21c465aea6ee94afc2
Toshiki <kuboy2482@gmail.com>
2020-10-28 21:19:55 +0900

    add test of TQC

---
+++ tests/algorithm/test_tqc.py
@@ -0,0 +1,20 @@
+import gym
+import pytest
+
+from rljax.algorithm.tqc import TQC
+
+from ._test_algorithm import _test_algorithm
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("d2rl", [(False), (True)])
+def test_sac(d2rl):
+    env = gym.make("MountainCarContinuous-v0")
+    algo = TQC(
+        num_agent_steps=100000,
+        state_space=env.observation_space,
+        action_space=env.action_space,
+        seed=0,
+        d2rl=d2rl,
+    )
+    _test_algorithm(env, algo)
