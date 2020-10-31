d2216f2ff7935da1e813cb12edbc4c9a76fd643e
Toshiki <kuboy2482@gmail.com>
2020-11-01 02:13:57 +0900

    release v0.0.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = "https://github.com/ku2482/rljax"
 EMAIL = "watanabe.toshiki@outlook.jp"
 AUTHOR = "Toshiki Watanabe"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 here = os.path.abspath(os.path.dirname(__file__))
 REQUIRED = open(os.path.join(here, "requirements.txt")).read().splitlines()
